af7ecc79d6bac127757a8fc9d140b0d99daa3c7e
ryosaito999 <ryocsaito@gmail.com>
2015-11-07 15:17:52 -0800

    pokemon List is done and filled with name, type and sprite

--- pokeCalls.js
+++ pokeCalls.js
@@ -1,17 +1,39 @@
 
+var pokemonList = [];
+var pEntry;
+
 function reqListener () {
-  var jsonText = this.responseText;
-  //console.log(jsonText);
 
-  var obj = JSON.parse(jsonText);
-  var pokemon = { name: obj.name, type: obj.types, sprite: "blue"};
-  console.log(pokemon);
+    var jsonText = this.responseText;
+ 	var obj = JSON.parse(jsonText);
+ 	var imgUrl = "http://pokeapi.co/media/img/" + obj.national_id.toString() + ".png";
+    var pokemon = { name: obj.name, type: [] , sprite: imgUrl };
+    console.log(pokemon);
 
+	for (i = 0; i < obj.types.length; i++) { 
+	     pokemon.type.push(obj.types[i].name);
+	}
+
+	window.pokemonList.push(pokemon);
 }
 
-var xhr = new XMLHttpRequest();
-xhr.addEventListener("load", reqListener);
+var url = "http://pokeapi.co/api/v1/pokemon/";
 
-xhr.open("GET", "http://pokeapi.co/api/v1/pokemon/1/");
-// Add your code below!
-xhr.send();
+for ( i = 1; i <= 25; i++){
+	counter = i;
+	var urlFull = url + i.toString() + "/";
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener("load", reqListener);
+	xhr.open("GET", urlFull);
+	// Add your code below!
+	xhr.send();
+}
+
+setTimeout(function(){
+    //console.log(pokemonList);
+    //modify pokemon list after this
+}, 5000);
+
+
+
+
