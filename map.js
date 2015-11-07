86d60fbe9cfbe6be0ad9f150b7a0922c8f5ea112
ryosaito999 <ryocsaito@gmail.com>
2015-11-07 11:04:03 -0800

    added map to the website
    Removed bootstrap will come back to it later

--- map.js
+++ map.js
@@ -1,29 +1,8 @@
 var map;
-var graphic;
-var currLocation;
-var watchId;
-
-require([
-"esri/map", "esri/geometry/Point", 
-"esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol",
-"esri/graphic", "esri/Color", "dojo/domReady!"
-], function(
-Map, Point,
-SimpleMarkerSymbol, SimpleLineSymbol,
-Graphic, Color
-) {
-map = new Map("map", {
-  basemap: "oceans",
-  center: [-85.957, 17.140],
-  zoom: 2
+require(["esri/map", "dojo/domReady!"], function(Map) {
+  map = new Map("mapDiv", {
+    center: [-56.049, 38.485],
+    zoom: 3,
+    basemap: "streets"
+  });
 });
-map.on("load", initFunc);
-
-function initFunc(map) {
-  if( navigator.geolocation ) {  
-    navigator.geolocation.getCurrentPosition(zoomToLocation, locationError);
-    watchId = navigator.geolocation.watchPosition(showLocation, locationError);
-  } else {
-    alert("Browser doesn't support Geolocation. Visit http://caniuse.com to see browser support for the Geolocation API.");
-  }
-}
