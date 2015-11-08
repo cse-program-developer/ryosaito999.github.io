36b1f1baae9e5180b4ece836b443ac2b952663a3
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2015-11-07 22:23:12 -0800

    Locate Button works

--- backup/map.js
+++ backup/map.js
@@ -2,17 +2,22 @@ var map;
 var graphic;
 var currLocation;
 var watchId;
+var geoLocate;
 
 require([
 "esri/map", "esri/geometry/Point", 
-"esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol",
-"esri/graphic", "esri/Color", "dojo/domReady!"
-], function( Map, Point, SimpleMarkerSymbol, SimpleLineSymbol,Graphic, Color) {
-map = new Map("mapDiv", {
-basemap: "oceans",
-center: [-85.957, 17.140],
-zoom: 2
-});
+"esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol", 
+"esri/layers/GraphicsLayer", "esri/graphic", "esri/Color", "esri/InfoTemplate",
+"esri/dijit/LocateButton", "dojo/domReady!"], 
+
+function( Map, Point, SimpleMarkerSymbol, SimpleLineSymbol,GraphicsLayer, Graphic, Color, InfoTemplate, LocateButton) {
+map = new Map("mapDiv", {basemap: "topo",center: [-117.3280644, 33.9737055], zoom: 16});
+
+geoLocate = new LocateButton({
+        map: map
+      }, "LocateButton");
+      geoLocate.setScale = false;
+      geoLocate.startup();
   
 map.on("load", initFunc);
 
@@ -23,7 +28,30 @@ function orientationChanged() {
   }
 }
 
-function initFunc(map) {
+function addPokemon(lon, lat){
+
+    p = new Point(lon, lat );
+    s =  new esri.symbol.PictureMarkerSymbol("images/pokeBall.png" , 51 , 51);
+    // can add a PictureMarkerSymbol here instead of marker Symbol
+    g = new Graphic(p, s);
+
+    // infoTemplate
+    var infoTemp = new InfoTemplate();
+    infoTemp.setTitle ("Pokemon Name");
+    infoTemp.setContent("Pokemon info");
+    g.setInfoTemplate(infoTemp.setTitle("Pokemon Name"));
+
+    map.graphics.add(g);
+
+
+}
+
+function initFunc(evt) {
+      
+    // var gl = new GraphicsLayer();
+  addPokemon(-117.325104, 33.978285 );
+
+
   if( navigator.geolocation ) {  
     navigator.geolocation.getCurrentPosition(zoomToLocation, locationError);
     watchId = navigator.geolocation.watchPosition(showLocation, locationError);
@@ -56,37 +84,36 @@ function locationError(error) {
   }
 }
 
-        function zoomToLocation(location) {
-          var pt = new Point(location.coords.longitude, location.coords.latitude);
-          addGraphic(pt);
-          map.centerAndZoom(pt, 12);
-        }
+    function zoomToLocation(location) {
+      var pt = new Point(location.coords.longitude, location.coords.latitude);
+      addGraphic(pt);
+      map.centerAndZoom(pt, 12);
+    }
 
-        function showLocation(location) {
-          //zoom to the users location and add a graphic
-          var pt = new Point(location.coords.longitude, location.coords.latitude);
-          if ( !graphic ) {
-            addGraphic(pt);
-          } else { // move the graphic if it already exists
-            graphic.setGeometry(pt);
-          }
-          map.centerAt(pt);
-        }
-        
-        function addGraphic(pt){
-          var symbol = new SimpleMarkerSymbol(
-            SimpleMarkerSymbol.STYLE_CIRCLE, 
-            12, 
-            new SimpleLineSymbol(
-              SimpleLineSymbol.STYLE_SOLID,
-              new Color([210, 105, 30, 0.5]), 
-              8
-            ), 
-            new Color([210, 105, 30, 0.9])
-          );
-          graphic = new Graphic(pt, symbol);
-          map.graphics.add(graphic);
-        }
-
+    function showLocation(location) {
+      //zoom to the users location and add a graphic
+      var pt = new Point(location.coords.longitude, location.coords.latitude);
+      if ( !graphic ) {
+        addGraphic(pt);
+      } else { // move the graphic if it already exists
+        graphic.setGeometry(pt);
+      }
+      map.centerAt(pt);
+    }
+    
+    function addGraphic(pt){
+      var symbol = new SimpleMarkerSymbol(
+        SimpleMarkerSymbol.STYLE_CIRCLE, 
+        12, 
+        new SimpleLineSymbol(
+          SimpleLineSymbol.STYLE_SOLID,
+          new Color([210, 105, 30, 0.5]), 
+          8
+        ), 
+        new Color([210, 105, 30, 0.9])
+      );
+      graphic = new Graphic(pt, symbol);
+      map.graphics.add(graphic);
+    }
 
 });
